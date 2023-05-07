03485038921e3131569f1b71714f06b5d819454c
Bhavin Patil <59190105+bhavinpatil@users.noreply.github.com>
2023-05-07 23:18:18 +0530

    Updated v0.1
    added signup and login page with collection of books in collection tab

--- Book Store Using React & SpringBoot/uttdemo/src/main/java/com/uttexample/uttdemo/service/BookService.java
+++ Book Store Using React & SpringBoot/uttdemo/src/main/java/com/uttexample/uttdemo/service/BookService.java
@@ -10,7 +10,6 @@ import com.uttexample.uttdemo.entity.Book;
 public class BookService implements BookServiceInterface {
 
 	ArrayList<Book> books;
-
 	public BookService() {
 		books = new ArrayList<>();
 		books.add(new Book(1010, "Harry Potter and the Prisoner of Azkaban", "J.K.Rowling", "https://m.media-amazon.com/images/I/81f7bXC-tTL.jpg"));
@@ -18,14 +17,12 @@ public class BookService implements BookServiceInterface {
 		books.add(new Book(1012, "Harry Potter and the Chamber of Secrets", "J.K.Rowling", "https://m.media-amazon.com/images/I/81S0LnPGGUL.jpg"));
 		books.add(new Book(1013, "Harry Potter and the Goblet of Fire", "J.K.Rowling", "https://m.media-amazon.com/images/I/810jKiNChxL.jpg"));
 		books.add(new Book(1014, "Harry Potter and the Order of the Phoenix", "J.K.Rowling", "https://m.media-amazon.com/images/I/81a4yXpXjnL.jpg"));
-
 		books.add(new Book(1015, "Harry Potter and the Half Blood Prince", "J.K.Rowling", "https://m.media-amazon.com/images/I/81p2+4nYtkL.jpg"));
 		books.add(new Book(1016, "The Catcher in the Rye", "J. D. Salinger", "https://m.media-amazon.com/images/I/91HPG31dTwL.jpg"));
 		books.add(new Book(1017, "The Fault in Our Stars", "John Green", "https://m.media-amazon.com/images/I/817tHNcyAgL.jpg"));
 		books.add(new Book(1018, "The Great Gatsby", "F.Scott Fitzgerald", "https://m.media-amazon.com/images/I/71FTb9X6wsL.jpg"));
 
 	}
-
 	@Override
 	public ArrayList<Book> getBooks() {
 		return books;
